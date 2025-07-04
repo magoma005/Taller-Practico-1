ae50fe5b6416e593e9cc10aa4d1f3259ed1f9fb0
Miguel Angel Guarin <114539021+magoma005@users.noreply.github.com>
2025-07-03 21:35:25 -0500

    Update AccesoExterno.java

--- src/externo/AccesoExterno.java
+++ src/externo/AccesoExterno.java
@@ -4,7 +4,7 @@ import mascota.Mascota;
 
 public class AccesoExterno {
     public static void main(String[] args) {
-        Mascota m = new Mascota("Nombre que quieras");
+        Mascota m = new Mascota("Nombre Aleatorio");
 
         System.out.println(m.nombrePublico); // OK
         m.metodoPublico(); // OK
